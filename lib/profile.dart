de137a5057ec574b9a4d1332520c12b22969accb
philemonyen <52116262+philemonyen@users.noreply.github.com>
2020-12-27 23:14:30 -0500

    Update profile design

--- lib/profile.dart
+++ lib/profile.dart
@@ -1,3 +1,5 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 
 class Profile extends StatefulWidget{
@@ -6,17 +8,42 @@ class Profile extends StatefulWidget{
 }
 
 class ProfileState extends State<Profile> {
-  final List<String> entries = <String>['Shoot', 'Rebound', 'Assist', 'Win Rate', 'Rating'];
+  final List<String> entries = <String>['POINTS', 'SHOOT', 'REBOUND', 'ASSIST', 'WIN RATE', 'RATING'];
+  List<int> attributes = [10, 0, 0, 0, 0, 0];
   Widget build(BuildContext context){
     return ListView.separated(
-      padding: const EdgeInsets.all(8),
+      padding: const EdgeInsets.all(20),
       itemCount: entries.length,
       itemBuilder: (BuildContext context, int index) {
-        return Container(
-          height: 50,
-          color: Colors.amber[600],
-          child: Center(child: Text('${entries[index]}')),
-        );
+        return ListTile(
+        leading: Text('${entries[index]}'),
+        title: Center(child: Text('${attributes[index]}'),),
+        tileColor: Colors.amber[600],
+        trailing: Wrap(
+            spacing: 10,
+            children: <Widget>[
+            IconButton(
+              icon: const Icon(Icons.add),
+              onPressed: () {
+                setState(() {
+                  if(attributes[0] > 0){
+                    attributes[index] += 1;
+                    attributes[0] -= 1;
+                  }
+                });
+              },),
+            IconButton(
+              icon: const Icon(Icons.remove),
+              onPressed: () {
+                setState(() {
+                  if(attributes[index] > 0){
+                    attributes[index] -= 1;
+                    attributes[0] += 1;
+                  }
+                });
+              },),
+          ]),
+      );
       },
       separatorBuilder: (BuildContext context, int index) => const Divider(),
     );
